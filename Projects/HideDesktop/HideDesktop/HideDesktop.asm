e4792428355325bceccbcf67837af4ca2090f27b
Max Shabarchin <maks.shabarchin@yandex.ru>
2021-05-11 13:18:14 +0300

    HideDesktop

---
+++ Projects/HideDesktop/HideDesktop/HideDesktop.asm
@@ -0,0 +1,26 @@
+; License link: https://github.com/Zalexanninev15/ALP/blob/main/LICENSE
+
+format PE GUI 4.0
+entry start
+include 'includes\win32a.inc'
+section '.data' data readable writeable
+szProg db 'ProgMan',0
+section '.code' code readable executable
+start:
+        invoke  FindWindow,szProg,NULL
+        invoke  GetWindow,eax,GW_CHILD
+        invoke  GetWindow,eax,GW_CHILD
+        mov     ebx,eax
+        invoke  IsWindowVisible,ebx
+        or      eax,eax
+        jne     @f
+        invoke  ShowWindow,ebx,SW_SHOW
+        jmp     loc_exit
+@@:
+        invoke  ShowWindow,ebx,SW_HIDE
+loc_exit:
+        invoke  ExitProcess,0
+section '.idata' import data readable writeable
+library kernel32,"kernel32.dll",\user32,"user32.dll"
+include "includes\api\kernel32.inc"
+include "includes\api\user32.inc"
